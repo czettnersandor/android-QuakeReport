2564bf2831a8a703091d502277df10ab60571dd3
Sándor Czettner <sandor@czettner.hu>
2016-08-27 12:50:35 +0100

    SettingsActivity

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -23,6 +23,8 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -40,6 +42,23 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         setContentView(R.layout.earthquake_activity);
 
         getSupportLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -76,7 +95,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         // TODO: Reset data in adapter
     }
 
-    private class EartquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+    public static class EartquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
 
         private String mUrl;
         public EartquakeLoader(Context context, String url) {
